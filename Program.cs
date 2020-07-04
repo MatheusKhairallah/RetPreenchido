d4bf60f1b8ee54f5f4ee10634ea5b80709c02d7d
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-07-03 21:29:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RetPreenchido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a Altura e Largura de retângulo entre 1 e 10");
+            Console.Write("Digite a largura: ");
+            int largura=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite a altura: ");
+            int altura=Convert.ToInt32(Console.ReadLine());
+
+            int valor1=1;
+            int valor2=1;
+
+            while (valor1<=altura)
+            {
+               while (valor2 <=largura)
+               {
+                   Console.Write("* ");
+                   valor2++;
+               }
+               valor2=1;
+               Console.WriteLine();
+               valor1++; 
+            }
+        }
+    }
+}
